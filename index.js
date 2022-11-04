5501d551d997210946b8c4bdc89fe0805f058e01
eklassen <eklass3@uwo.ca>
2022-11-03 20:08:56 -0400

    Set up MySQL connection.

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const express = require('expresss');
+const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
-app.get();
+const connection = initDB(mysql);
+
+connection.query('SELECT * FROM genres WHERE genre_id = ' + 2, function(error, results, fields) {
+    if (error) throw error;
+  console.log('The solution is: ', results[0].title);
+});
+
+function initDB(sql) {
+   
+    let connection = sql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'K983pj*B',
+        database: 'music'
+    });
+    
+    connection.connect();
+
+    console.log("DB Connected");
+
+    return connection;
+}
