e4aed55f653827c70b6a221832fdbf5085f2fff1
eklassen <eklass3@uwo.ca>
2022-11-05 12:52:46 -0400

    All backend requirements done, excluding input sanitization.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const e = require('express');
 const app = express();
 
 const connection = initDB(mysql);
+
+const jsonParser = bodyParser.json();
 
 //Get genres enpoint. Backend requirement #1
 app.get('/api/genres', (req, res) => {
@@ -49,6 +53,134 @@ app.get('/api/artists/search/:search', (req, res) => {
     connection.query('SELECT artist_id FROM tracks WHERE artist_name LIKE \'%' + search + '%\' LIMIT 10;', function(error, results, fields) {
         if (error) throw error;
       res.send(JSON.stringify(results));
+    });
+});
+
+//Create list. Backend requirement #6
+app.post('/api/lists', jsonParser, (req, res) => {
+   let list_name = req.body.list_name;
+    console.log(req.body);
+   // TODO PERFORM SANITIZATION
+    connection.query('INSERT INTO lists (list_name) VALUES (\'' + list_name + '\')', function(error, results, fields) {
+        if (error) {
+            res.status(400).send(error.sqlMessage);
+        } else {
+            res.status(200).send("Successfully created new list");
+        }
+    });
+});
+
+//Add tracks to list. Backend requirement #7
+/*
+Example input:
+{"track_ids": [{"track_id": 1},
+{"track_id": 2},
+{"track_id": 21}]}
+*/
+app.post('/api/lists/:list', jsonParser, (req, res) => {
+    let list_name = req.params.list;
+    let track_ids = req.body.track_ids;
+     console.log(req.body);
+    // TODO PERFORM SANITIZATION
+
+    //Step 1, clear all old tracks saved to this list.
+     connection.query('DELETE FROM saved_tracks WHERE list_name = \'' + list_name + '\'', function(error, results, fields) {
+         if (error) throw error;
+        //Step 2, Check if list exists.            
+         connection.query('SELECT * FROM lists WHERE list_name = \'' + list_name + '\'', function(error, results, fields) {
+            if (error) throw error;
+        
+            if (results.length > 0) {
+                //Step 3, insert new tracks
+                track_ids.forEach(id => {
+                    connection.query('INSERT INTO saved_tracks VALUES (\'' + list_name + '\', ' + id.track_id + ');', function(error, results, fields) {
+                        if (error) throw error;
+                        
+                    });
+                });
+
+                res.send("Successfully created populated list " + list_name + " with tracks.");
+            } else {
+                res.status(400).send("A list with that name does not exist.");
+            }
+         });
+     });
+ });
+
+ //Get list of track ids list. Backend requirement #8
+app.get('/api/lists/tracks/:list', jsonParser, (req, res) => {
+    let list_name = req.params.list;
+    // TODO PERFORM SANITIZATION
+     connection.query('SELECT * FROM saved_tracks WHERE list_name = \'' + list_name + '\'', function(error, results, fields) {
+        if (error) throw error;
+        
+        res.send(JSON.stringify(results));
+     });
+ });
+
+  //Get list of track ids list. Backend requirement #9
+app.delete('/api/lists/:list', jsonParser, (req, res) => {
+    let list_name = req.params.list;
+    // TODO PERFORM SANITIZATION
+    connection.query('SELECT * FROM lists WHERE list_name = \'' + list_name + '\'', function(error, results, fields) {
+        if (error) throw error;
+        if (results.length === 0)
+            res.status(400).send("List does not exist.");
+        else {
+            connection.query('DELETE FROM lists WHERE list_name = \'' + list_name + '\'', function(error, results, fields) {
+                if (error) throw error;
+                connection.query('DELETE FROM saved_tracks WHERE list_name = \'' + list_name + '\'', function(error, results, fields) {
+                    if (error) throw error;
+
+                    res.status(200).send("Successfully deleted " + list_name);
+                });
+            });
+        }
+    });
+});
+
+ //Get list details. Backend requirement #10
+app.get('/api/lists', jsonParser, (req, res) => {
+    // TODO PERFORM SANITIZATION
+    //Step 1, select all list names
+     connection.query('SELECT count(t.track_id), t.list_name FROM saved_tracks t INNER JOIN lists l ON t.list_name = l.list_name GROUP BY t.list_name;', function(error, results, fields) {
+        if (error) throw error;
+    
+
+        let trackCountName = results;
+
+        connection.query('SELECT t.track_duration, l.list_name FROM saved_tracks l INNER JOIN tracks t ON l.track_id = t.track_id;', function(error, results, fields) {
+            if (error) throw error;
+
+            let trackDurationName = results;
+
+            for (let i = 0; i < trackCountName.length; i++) {
+                let tcn = trackCountName[i];
+                tcn.total_duration = 0;
+                for (let j = 0; j < trackDurationName.length; j++) {
+                    let tdn = trackDurationName[j];
+
+                    console.log(tdn.track_duration);
+
+                    let a = tdn.track_duration.split(':');
+                    let totalSec = parseInt(a[0])*60 + parseInt(a[1]);
+                    let track_duration = totalSec;
+                    console.log(totalSec);
+
+                     if (tcn.list_name === tdn.list_name) {
+                         tcn.total_duration += track_duration;
+                     }
+                }
+                let date = new Date(null);
+                date.setSeconds(tcn.total_duration);
+                tcn.total_duration = date.toISOString().substring(11, 19);
+            }
+
+            res.send(trackCountName);
+
+        });
+
+        
     });
 });
 
