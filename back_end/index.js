fe5ffdc81ddc4640766ae2c950855f5bc8d595ea
cthom72 <71096617+cthom72@users.noreply.github.com>
2022-11-30 17:03:06 -0500

    Authorization + Authentication Now Works Succesfully
    JWT Token is now decrypted and displays in console. Access to a protected path is now possible by sending JWT token to an authorization API function inside api.js ... the sample protected path is available in backend/index.js

--- back_end/index.js
+++ back_end/index.js
@@ -4,34 +4,21 @@ var oAuth = require("./middleware/oAuth");
 var port = process.env.PORT || 3001;
 const app = express();
 const cors = require('cors');
-const jwtCheck = require("./api/api.js");
+const {checkJwt, checkAdmin} = require("./api/api.js");
 app.use("/challenges", oAuth);
 
 app.get("/challenges", async (req, res) => {
-  console.log('Challenges accessed..')
-    try {
-      const { access_token } = req.oauth;
-      console.log(access_token);
-      /*const response = await axios({
-        method: "get",
-        url: 'http://localhost:3000/api',
-        headers: { Authorization: `Bearer ${access_token}` },
-      });*/
-      console.log('test')
-      res.json(response.data);
-    } catch (error) {
-      console.log(error);
-      if (error.response === 401) {
-        res.status(401).json("Unauthorized to access data");
-      } else if (error.response === 403) {
-        res.status(403).json("Permission denied");
-      } else {
-        res.status(500).json("Whoops, something went wrong");
-      }
-    }
+  var {access_token} = req.oauth;
+  const response = await axios({
+    method:"get",
+    url:'http://localhost:3000/protected',
+    headers: { Authorization: `Bearer ${access_token}` },
   });
-  app.get('/api',jwtCheck,(req,res,next)=>{
-    next();
+  console.log("Token: " + req.oauth.access_token);
+  res.json(response.data);
+});
+  app.get('/protected',checkJwt,(req,res)=>{
+    res.json('You are in a protected path! See console for token.')
   })
   app.listen(port, () => console.log("Started"));
 
