c8e92888eed070da90cd928775fae76f7eb6ef0d
bdavidov8771 <bdavidov@uwo.ca>
2022-12-07 02:10:16 -0500

    Add playlist adding functionality

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -17,12 +17,13 @@ import Button from "./components_unauthenticated/Button";
 import { useEffect, useState } from "react";
 import TrackDetails from "./components_unauthenticated/TrackDetails";
 import PlaylistDetails from "./components_unauthenticated/PlaylistDetails";
+import queryString from "query-string";
 
 let search = "";
 
 const ROOT = "http://localhost:3000";
 
-function App() {
+function App({}) {
 
   const [searchResults, setSearchResults] = useState([]);
   const [playlists, setPlaylists] = useState([]);
@@ -33,7 +34,6 @@ function onSearch() {
   fetch(query)
   .then((response) => response.json())
   .then((data) => {
-      console.log(data);
       setSearchResults(data);
   })
   .catch(function (error) {
@@ -42,18 +42,16 @@ function onSearch() {
 }
 
 useEffect(()=>{
-  console.log("Searching");
   const query = `${ROOT}/api/public/lists/0`;
   fetch(query)
   .then((response) => response.json())
   .then((data) => {
-      console.log(data);
       setPlaylists(data);
   })
   .catch(function (error) {
 
   });
-}, playlists)
+}, [playlists])
 
 function onSearchChange(event) {
   console.log(event.target.value);
@@ -70,6 +68,8 @@ function onSearchChange(event) {
           <LoginButton />
           <LogoutButton />
           <p style={{color: 'black'}}>What this site offers: hundreds of thousands of artists and music across hundreds of genres. Build and review playlists with an account. Login to start.</p>
+          <Route path="/home/authenticated" component={Sidebar} />
+          <br/>
           <div style={{display: 'flex', flexDirection: 'row'}}>
             <SearchUnauth onChange={(event) => onSearchChange(event)} placeholder={"Search for artist, genre, or track title"} style={{width: 500}}/>
             <button onClick={()=>onSearch()}>Search</button>
@@ -90,18 +90,16 @@ function onSearchChange(event) {
                 <PlaylistDetails playlist={playlist}/>
               )
             })}
-
             <p style={{color: 'black'}}>View our Privacy Policy <a target="_blank" href="https://docs.google.com/document/d/185wpPyn-yYgTk0jM1wedR7XnTRMPpiRSYPGkB5xdEmY/edit?usp=sharing">here.</a></p>
             <p style={{color: 'black'}}>View our Acceptable Use Policy <a target="_blank" href="https://docs.google.com/document/d/1c5Zeu4AZdyc1UYLKYbTsfP-IwXRqBClBpEWbeKL1OHI/edit?usp=sharing">here.</a></p>
             <p style={{color: 'black'}}>View our DMCA Take Down Policy <a target="_blank" href="https://docs.google.com/document/d/1kABt3cbdyhT62ElTbuTFtq3i1ucOKBDRHBwcfJR-bFY/edit?usp=sharing">here.</a></p>
         </span>
       </div>
-      <Route path="/home" component={Sidebar} />
       <Route path="/home/search" component={Search}/>
       <Route path="/home/search" component={TrackHolder}/>
       <Route path="/home/search" component={PlaylistContainer}/>
-      <Route path="/home/library" component={Library}/>
-      <Route path="/home/playlist" exact component={Playlist}/>
+      <Route path="/home/authenticated/library" component={Library}/>
+      <Route path="/home/authenticated/playlist" component={Playlist}/>
       <Route path="/home/playlist/id" component={PlaylistData}/>
       <Route path="/test" component={Test}/>
     </div>
--- front_end/src/components_authenticated/Playlist.js
+++ front_end/src/components_authenticated/Playlist.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useState, useEffect} from 'react';
 import "./Playlist.css"
 
 const Playlist = () => {
-    const addPlaylist = async () => {
+    const [playlistName, setPlaylistName] = useState('')
+    const [description, setDescription] = useState('')
+    const [checked, setChecked] = useState(true)
+
+    const addPlaylist = async() => {
+        const check = checked ? 1 : 0
         fetch(`/api/protected/lists`, {
             method: 'POST',
             headers: {
-            Authorization: `Bearer ${sessionStorage.getItem("jwt")}`
+            Authorization: `Bearer ${sessionStorage.getItem("jwt")}`,
+            'Content-type': 'application/json'
             },
             body: {
-                description: `test`,
-                public: '0',
-                list_name: 'sample name',
+                description: description,
+                public: check,
+                list_name: playlistName,
             }
         })
         .then(res => res.json())
@@ -19,11 +26,22 @@ const Playlist = () => {
             console.log(res);
         })
     }
+
+    const handlePlaylistName = (e) => {
+        setPlaylistName(e.target.value);
+    }
+    const handleDescription = (e) => {
+        setDescription(e.target.value);
+    }
+    const handleChecked = (e) => {
+        setChecked(e.target.checked);
+    }
+
     return(
     <form className="add-playlist">
-        <div className="item"><span>Playlist Name</span><input placeholder="Name"></input></div>
-        <div className="item"><span>Descripton</span><input placeholder="Description"></input></div>
-        <div className="item"><span>Private Playlist</span><input type="checkbox" defaultChecked={true}></input></div>
+        <div className="item"><span>Playlist Name</span><input placeholder="Name" onChange={handlePlaylistName}/></div>
+        <div className="item"><span>Descripton</span><input placeholder="Description" onChange={handleDescription}/></div>
+        <div className="item"><span>Private Playlist</span><input type="checkbox" defaultChecked={true} onChange={handleChecked}/></div>
         <div className='item'><button id='add-button' onClick={() => addPlaylist()}>Add</button></div>
     </form>
     )
